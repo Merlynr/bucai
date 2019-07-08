226e1b7b2cff8ce0c9ed31672b92c89b4dde259e
merlynr <lcq1013962426@gmail.com>
2019-07-08 23:44:55 +0800

    环境搭建完成

--- app/index.js
+++ app/index.js
@@ -1,13 +1,23 @@
 const { app, BrowserWindow } = require('electron');
+/* app/index.js */
+const isDev = require('electron-is-dev');
+const path = require('path');
 
 let win;
 
+const devUrl = 'http://localhost:3000';
+// 本地文件路径定位到打包的react文件
+const localUrl = `file://${path.resolve(
+  __dirname,
+  '../../app.asar/build'
+)}/index.html`;
+const appUrl = isDev ? devUrl : localUrl;
 function createWindow() {
   // 创建浏览器窗口。
   win = new BrowserWindow({ width: 800, height: 600 });
 
   // 然后加载应用的 index.html。
-  win.loadURL('http://localhost:3000');
+  win.loadURL(appUrl);
 
   // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
@@ -28,4 +38,28 @@ app.on('activate', () => {
   if (win === null) {
     createWindow();
   }
+});
+
+// 利用electron-debug，添加和Chrome类似的快捷键
+isDev && require('electron-debug')({ enabled: true, showDevTools: false });
+// 用于添加Chromium插件
+function createDevTools() {
+  const {
+    default: installExtension,
+    REACT_DEVELOPER_TOOLS,
+    REDUX_DEVTOOLS,
+  } = require('electron-devtools-installer');
+  // 安装devtron
+  const devtronExtension = require('devtron');
+  devtronExtension.install();
+  // 安装React开发者工具
+  installExtension(REACT_DEVELOPER_TOOLS);
+  installExtension(REDUX_DEVTOOLS);
+}
+
+// 修改应用启动事件
+app.on('ready', () => {
+  createWindow();
+  // 只在开发环境加载开发者工具
+  isDev && createDevTools();
 });
